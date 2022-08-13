a9cafa8b99fda2c2dac84cf4a58dc215139eb84f
Michael Shaffer <michaelshaffer45@gmail.com>
2022-08-13 19:13:11 -0400

    first

---
+++ hermes/hermesApp.swift
@@ -0,0 +1,17 @@
+//
+//  hermesApp.swift
+//  hermes
+//
+//  Created by Michael Shaffer on 8/13/22.
+//
+
+import SwiftUI
+
+@main
+struct hermesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
